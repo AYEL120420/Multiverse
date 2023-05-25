008a9c67bd39322ee450a35db67ed672c6143db3
elaissaouir <elaissaouiraounak0@gmail.com>
2023-05-25 11:25:34 +0200

    ma nouvelle commit

--- Multiverse/Program.cs
+++ Multiverse/Program.cs
@@ -6,7 +6,7 @@ namespace Multiverse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Master");
+            Console.WriteLine("Hello");
             Console.WriteLine("Hello World! Master");
         }
     }
